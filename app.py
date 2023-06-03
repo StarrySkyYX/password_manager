1f3131a92e8b5929442ea1b43eeae602800e8a49
StarrySkyYX <132223842+StarrySkyYX@users.noreply.github.com>
2023-06-03 17:41:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+from flask import Flask
 from flask import request
 from flask import render_template
 # from user import user
 import sqlite3
+app = Flask(__name__)
 conn = sqlite3.connect('data/password_manager.db')
 c=conn.cursor()
 login_user={}
 # closs
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     # 
@@ -20,14 +23,17 @@ def login():
             return True
         return False
     # 
+    error=""
+    user_mail=request.form['user_mail']
+    password=request.form['password']
     if request.method=="POST":
-        if check(request.form['user_mail'],request.form['password']):
-            login_user[request.form['user_mail']]=user.search(request.form['user_mail'])
-            return render_template("home.html", login_user[request.form['user_mail']].name)
+        if check(user_mail,password):
+            login_user[user_mail]=user.search(user_mail)
+            return render_template("home.html", login_user[user_mail].name)
         else:
             error="無效的使用者名稱/密碼"
     return render_template('login.html',error=error)
-
+# 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     # 
@@ -52,26 +58,43 @@ def register():
         c.execute(add_sql_table, (user_mail,))
         conn.commit()
     # 
-    # def insert_sql_Users(user_mail,password):
-    #     return
+    def insert_Users(user_mail,password):
+        insert_user='''INSERT INTO Users VALUES (?,?)'''
+        c.execute(insert_user,(user_mail,password,))
+        conn.commit()
 
-    # 
+    # 一開始if語句嵌套過多層，可讀性差，詢問chatGPT解決辦法，他提議以elif來改善
+    error=""
     if request.method=="POST":
-        if check(request.form['user_mail']):
-            if request.form['password']==request.form['confirm_password']:
-                if request.form['user_name']!="":
-                    login_user[request.form['user_mail']]=user(name=request.form['user_name'])
-                    insert_sql_Users(request.form['user_mail'],request.form['password'])
-                    add_table(request.form['user_mail'])
-                    return render_template("home.html", login_user[request.form['user_mail']].name)
-                else:
-                    error="使用者名稱不得空白"
-            else:
-                error="密碼不一致"
+        user_mail = request.form['user_mail']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        user_name = request.form['user_name']
+        if user_mail=="":
+            error="電子郵件不得空白"
+        elif not check(request.form['user_mail']):
+            error="電子郵件已註冊"
+        elif password=="":
+            error="密碼不得空白"
+        elif password!=confirm_password:
+            error="密碼不一致"
+        elif user_name=="":
+            error="使用者名稱不得空白"
         else:
-            error="使用者已註冊"
+            login_user[request.form['user_mail']]=user(name=request.form['user_name'])
+            insert_Users(request.form['user_mail'],request.form['password'])
+            add_table(request.form['user_mail'])
+            return render_template("home.html", login_user[request.form['user_mail']].name)
     return render_template('register.html',error=error)
+    
+# 
+@app.route('/edit', methods=['POST', 'GET'])
+def edit():
+    
+    
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
