0ff13bc24d6450448d5e3cec91dad8dcf7bc888d
StarrySkyYX <132223842+StarrySkyYX@users.noreply.github.com>
2023-06-06 22:52:42 +0800

    Update user.py

--- user.py
+++ user.py
@@ -13,7 +13,8 @@ class User:
     def __init__(self,user_mail):
         self.mail=user_mail
         query='''SELECT name FROM Users WHERE mail LIKE ?'''
-        self.name=cursor.execute(query, (user_mail,))
+        result=cursor.execute(query, (user_mail,)).fetchall()
+        self.name=result[0]
 
     def search(self,search_str):
         rows=self.load
@@ -22,25 +23,25 @@ class User:
                 return row
 
     def load(self):
-        query='''SELECT * FROM ?'''
+        query='''SELECT * FROM {self.name}'''
         cursor.execute(query,(self.mail,))
         rows=cursor.fetchall()
         # 詢問如何把搜尋結果轉成字典
         return [dict(row) for row in rows] 
     
     def delete(self,delete_keyword):
-        delete_sql_row='''DELETE FROM Users WHERE name=?'''
+        delete_sql_row='''DELETE FROM {self.name} WHERE name=?'''
         cursor.execute(delete_sql_row,(delete_keyword,))
         conn.commit()
         
     def add(self,keyword,account_id,account_password):
-        insert_user='''INSERT INTO ? VALUES (?,?,?)'''
-        cursor.execute(insert_user,(self.mail,keyword,account_id,account_password,))
+        insert_user='''INSERT INTO {self.name} VALUES (?,?,?)'''
+        cursor.execute(insert_user,(keyword,account_id,account_password,))
         conn.commit()
 
     def edit(self,keyword,account_id,account_password):
-        edit_sql_str='''UPDATE ? SET id = ? password = ? WHERE name = ?'''
-        cursor.execute(edit_sql_str,(self.mail,account_id,account_password,keyword,))
+        edit_sql_str='''UPDATE {self.name} SET id = ? password = ? WHERE name = ?'''
+        cursor.execute(edit_sql_str,(account_id,account_password,keyword,))
         conn.commit()
 
     @staticmethod
@@ -56,11 +57,21 @@ class User:
         return False
     
     @staticmethod
-    def check_exist(user_mail):
+    def check_mail_exist(user_mail):
         # 定義查詢語句和佔位符
         query = '''SELECT mail FROM Users WHERE mail LIKE ?'''
         # 執行查詢，將具體值綁定到佔位符
         cursor.execute(query, (user_mail,))
+        if len(cursor.fetchall())==0:
+            return True
+        return False
+    
+    @staticmethod
+    def check_name_exist(user_name):
+        # 定義查詢語句和佔位符
+        query = '''SELECT name FROM Users WHERE name LIKE ?'''
+        # 執行查詢，將具體值綁定到佔位符
+        cursor.execute(query, (user_name,))
         if len(cursor.fetchall())==0:
             return True
         return False
@@ -71,14 +82,14 @@ class User:
         cursor.execute(insert_user,(user_mail,password,user_name,))
         conn.commit()
     @staticmethod
-    def add_table(user_mail):
+    def add_table(user_name):
         add_sql_table='''
-        CREATE TABLE ? (
+        CREATE TABLE {name} (
             "name"  TEXT,
             "id"    TEXT,
             "password"  TEXT,
             PRIMARY KEY("name")
             )
         '''
-        cursor.execute(add_sql_table, (user_mail,))
+        cursor.execute(add_sql_table)
         conn.commit()
