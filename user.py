d397f23956c54a12a41060503438a7317112d647
StarrySkyYX <132223842+StarrySkyYX@users.noreply.github.com>
2023-06-08 12:51:46 +0800

    補注解

--- user.py
+++ user.py
@@ -1,125 +1,139 @@
 import sqlite3
-import atexit
-conn = sqlite3.connect('data/password_manager.db')
-cursor=conn.cursor()
-# gpt
-def close_db_conn():
-    conn.close()
-atexit.register(close_db_conn)
 
 class User:
-    name=""
-    mail=""
     def __init__(self,user_mail):
-        conn = sqlite3.connect('data/password_manager.db')
-        cursor=conn.cursor()
-        self.mail=user_mail
-        query='''SELECT name FROM Users WHERE mail=?'''
-        self.name=''.join(cursor.execute(query, (self.mail,)).fetchall()[0])
-        
-    
-<<<<<<< HEAD
-=======
-    # 參數化查詢的改進版本
->>>>>>> a4c188747c40a0df4ec0190ed5fe7641daff7910
-    def delete(self, delete_keyword):
-        # By GPT_0608
+        """Initialize the User object.
+        :param user_mail: User's mail
+        """
+        # Get the user name when initializing the User object
         with sqlite3.connect('data/password_manager.db', check_same_thread=False) as conn:
-        cursor = conn.cursor()
-        delete_sql_row = '''DELETE FROM {} WHERE name = ?;'''.format(self.name)
-        cursor.execute(delete_sql_row, (delete_keyword,))
-<<<<<<< HEAD
-=======
->>>>>>> a4c188747c40a0df4ec0190ed5fe7641daff7910
+            cursor=conn.cursor()
+            self.mail=user_mail
+            query='''SELECT name FROM Users WHERE mail=?'''
+            self.name=''.join(cursor.execute(query, (self.mail,)).fetchall()[0])
+            
+    def delete(self,delete_keyword):
+        """Delete the ID and password stored by the user.
+        :param delete_keyword: The name of the ID and password to be deleted by the User.
+        """
+        with sqlite3.connect('data/password_manager.db', check_same_thread=False) as conn:
+            cursor=conn.cursor()
+            delete_sql_row='''DELETE FROM {} WHERE name=?;'''.format(self.name)
+            cursor.execute(delete_sql_row,(delete_keyword,))
         
     def add(self,account_name,account_id,account_password):
-        conn = sqlite3.connect('data/password_manager.db')
-        cursor=conn.cursor()
-        insert_user='''INSERT INTO {} VALUES (?,?,?)'''.format(self.name)
-        cursor.execute(insert_user,(account_name,account_id,account_password,))
-        conn.commit()
-
-
-    def edit(self,account_name,account_id,account_password):
-        # By GPT_0608
+        """Add the ID and password stored by the user.
+        :param account_name: The name of the account to be added by the User.
+        :param account_id: The ID of the account to be added by the User.
+        :param account_name: The password of the account to be added by the User.
+        """
         with sqlite3.connect('data/password_manager.db', check_same_thread=False) as conn:
-        cursor = conn.cursor()
-        edit_sql_str = '''UPDATE {} SET id = ?, password = ? WHERE name = ?;'''.format(self.name)
-        cursor.execute(edit_sql_str, (account_id, account_password, account_name,))
-
+            cursor=conn.cursor()
+            insert_user='''INSERT INTO {} VALUES (?,?,?)'''.format(self.name)
+            cursor.execute(insert_user,(account_name,account_id,account_password,))
     
-
+    def edit(self,account_name,account_id,account_password):
+        """Edit the ID and password stored by the user.
+        :param account_name: The name of the account to be added by the User.
+        :param account_id: The ID of the account to be added by the User.
+        :param account_name: The password of the account to be added by the User.
+        """
+        with sqlite3.connect('data/password_manager.db', check_same_thread=False) as conn:
+            cursor=conn.cursor()
+            edit_sql_str='''UPDATE {} SET id = ?, password = ? WHERE name = ?;'''.format(self.name)
+            cursor.execute(edit_sql_str,(account_id,account_password,account_name,))
+       
     @staticmethod
     def check_login(user_mail,password):
-        conn = sqlite3.connect('data/password_manager.db')
-        cursor=conn.cursor()
-        # 定義查詢語句和佔位符
-        query = '''SELECT password FROM Users WHERE mail LIKE ?'''
-        result=cursor.execute(query, (user_mail,)).fetchall()
-        if len(result)==0:
+        """Check user login
+        :param user_mail: Email inputed by user
+        :param password: password inputed by user
+        """
+        with sqlite3.connect('data/password_manager.db', check_same_thread=False) as conn:
+            cursor=conn.cursor()
+            query = '''SELECT password FROM Users WHERE mail LIKE ?'''
+            result=cursor.execute(query, (user_mail,)).fetchall()
+            if len(result)==0:
+                return False
+            check_password =''.join(result[0])
+            if check_password==password:
+                return True
             return False
-        check_password =''.join(result[0])
-        if check_password==password:
-            return True
-        return False
     
     @staticmethod
     def check_mail_exist(user_mail):
-        conn = sqlite3.connect('data/password_manager.db')
-        cursor=conn.cursor()
-        # 定義查詢語句和佔位符
-        query = '''SELECT mail FROM Users WHERE mail LIKE ?'''
-        # 執行查詢，將具體值綁定到佔位符
-        cursor.execute(query, (user_mail,))
-        if len(cursor.fetchall())==0:
-            return True
-        return False
+        """Check if User exists
+        :param user_mail: Email inputed by user
+        """
+        with sqlite3.connect('data/password_manager.db', check_same_thread=False) as conn:
+            cursor=conn.cursor()
+            # 定義查詢語句和佔位符
+            query = '''SELECT mail FROM Users WHERE mail LIKE ?'''
+            # 執行查詢，將具體值綁定到佔位符
+            cursor.execute(query, (user_mail,))
+            if len(cursor.fetchall())==0:
+                return True
+            return False
     
     @staticmethod
     def check_name_exist(user_name):
-        conn = sqlite3.connect('data/password_manager.db')
-        cursor=conn.cursor()
-        # 定義查詢語句和佔位符
-        query = '''SELECT name FROM Users WHERE name LIKE ?'''
-        # 執行查詢，將具體值綁定到佔位符
-        cursor.execute(query, (user_name,))
-        if len(cursor.fetchall())==0:
-            return True
-        return False
+        """Check if the name be used.
+        :param user_mail: Email inputed by user
+        """
+        with sqlite3.connect('data/password_manager.db', check_same_thread=False) as conn:
+            cursor=conn.cursor()
+            # 定義查詢語句和佔位符
+            query = '''SELECT name FROM Users WHERE name LIKE ?'''
+            # 執行查詢，將具體值綁定到佔位符
+            cursor.execute(query, (user_name,))
+            if len(cursor.fetchall())==0:
+                return True
+            return False
     
     @staticmethod
     def insert_user(user_mail,password,user_name):
-        conn = sqlite3.connect('data/password_manager.db')
-        cursor=conn.cursor()
-        insert_user='''INSERT INTO Users VALUES (?,?,?)'''
-        cursor.execute(insert_user,(user_mail,password,user_name,))
-        conn.commit()
+        """Insert new User into SQL
+        :param user_mail: Email of the new User.
+        :param password: Password of the new User.
+        :param user_name: Name of the new User.
+        """
+        with sqlite3.connect('data/password_manager.db', check_same_thread=False) as conn:
+            cursor=conn.cursor()
+            insert_user='''INSERT INTO Users VALUES (?,?,?)'''
+            cursor.execute(insert_user,(user_mail,password,user_name,))
+
     @staticmethod
     def add_table(user_name):
-        conn = sqlite3.connect('data/password_manager.db')
-        cursor=conn.cursor()
-        add_sql_table='''
-        CREATE TABLE {} (
-            "name"  TEXT NOT NULL,
-            "id"     TEXT NOT NULL,
-            "password"   TEXT NOT NULL,
-            PRIMARY KEY("name")
-            )
-        '''.format(user_name)
-        cursor.execute(add_sql_table)
-        conn.commit()
+        """Create new table into SQL for the new user
+        :param user_name: Name of the new user.
+        """
+        with sqlite3.connect('data/password_manager.db' ,check_same_thread=False) as conn:
+            cursor=conn.cursor()
+            add_sql_table='''
+            CREATE TABLE {} (
+                "name"  TEXT NOT NULL,
+                "id"     TEXT NOT NULL,
+                "password"   TEXT NOT NULL,
+                PRIMARY KEY("name")
+                )
+            '''.format(user_name)
+            cursor.execute(add_sql_table)
+
     @staticmethod   
-    def load(name):
-        conn = sqlite3.connect('data/password_manager.db')
-        cursor = conn.cursor()
-        query = '''SELECT * FROM {}'''.format(name)
-        cursor.execute(query)
-        rows = cursor.fetchall()
-        result=[]
-        for row_tuple in rows:
-            result.append({
-                "name":row_tuple[0],
-                "id":row_tuple[1],
-            "password":row_tuple[2]
-                                })
-        return result
+    def load(user_name):
+        """Load user stored information.
+        :param user_name: Name of the user.
+        """
+        with sqlite3.connect('data/password_manager.db', check_same_thread=False) as conn:
+            cursor=conn.cursor()
+            query = '''SELECT * FROM {}'''.format(user_name)
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            result=[]
+            for row_tuple in rows:
+                result.append({
+                    "name":row_tuple[0],
+                    "id":row_tuple[1],
+                "password":row_tuple[2]
+                                    })
+            return result
